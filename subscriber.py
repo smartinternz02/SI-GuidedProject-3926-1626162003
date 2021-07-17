301be22ac1ac776bb2bd0986d19b90f7902a6ee7
shalini2001 <66840714+shalini2001@users.noreply.github.com>
2021-07-17 21:24:43 +0530

    Delete subscriber.py

--- subscriber.py
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env python
-import rospy
-from std_msgs.msg import String
-def callback(msg):
-    print msg.data
-
-rospy.init_node('subscriber')
-sub=rospy.Subscriber('counter', String, callback)
-rospy.spin()
